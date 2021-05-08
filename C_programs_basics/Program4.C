bbdbfe50d377c6ce6b735f135b8a255caa24b048
Neevea8880 <56985164+Neevea8880@users.noreply.github.com>
2021-05-08 13:24:23 +0530

    Update Program4.C

--- C_programs_basics/Program4.C
+++ C_programs_basics/Program4.C
@@ -1,15 +1,12 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int i,n1,n2;
-clrscr();
 printf("enter the first and second no:");
 scanf("%d%d",&n1,&n2);
 for(i=n1;i<=n2;i++)
     {
     if(i%2!=0)
-      printf("%d",i);
+      printf("%d ",i);
     }
-getch();
 }
