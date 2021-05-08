c9b06391930f6e77f9f19bc7ccc479a22938e9ae
Neevea8880 <56985164+Neevea8880@users.noreply.github.com>
2021-05-08 13:52:41 +0530

    Update and rename Program11.C to Program9.C

--- C_programs_basics/Program9.C
+++ C_programs_basics/Program9.C
@@ -1,16 +1,13 @@
 #include<stdio.h>
-#include<conio.h>
 #include<math.h>
 void main()
 {
  float n,r;
- clrscr();
- printf("enter value ofn in degree:");
+ printf("enter value of n in degree:");
  scanf("%f",&n);
   r=n*3.14/180;
  printf("\n sin value: %f",sin(r));
  printf("\n cos value: %f",cos(r));
- getch();
  }
 
 
