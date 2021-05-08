54d416abe63c308d776aeaf8abbea00e37a38538
Neevea8880 <56985164+Neevea8880@users.noreply.github.com>
2021-05-08 13:40:26 +0530

    Update Program7.C

--- C_programs_basics/Program7.C
+++ C_programs_basics/Program7.C
@@ -1,28 +1,9 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-int i,j,row,space,ncr;
-printf("enter no of rows:");
-scanf("%d",&row);
-for(i=0;i<row;i++)
-{
- for(space=1;space<row-i;space++)
-   printf(" ");
- for (j=0;j<=i;j++)
-  {
-   if(i==0||j==0)
-   {
-   ncr=i;
-   printf(" %d ",ncr);
-   }
-   else
-   {
-   ncr*=(i-j+1)/j;
-   printf(" %d ",ncr);
-   }
-  }
- printf("/n");
- }
- getch();
- }
+  int n,i;
+  printf("enter a no:");
+  scanf("%d",&n);
+  for(i=1;i<=10;i++)
+  printf("%d*%d=%d\n",i,n,i*n);
+}
